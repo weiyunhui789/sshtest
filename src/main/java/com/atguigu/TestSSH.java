50a31ea883f04f0f50fab76de90e9c981413625f
weiyunhui <weiyunhui@atguigu.cn>
2023-12-23 15:46:13 +0800

    ssh

---
+++ src/main/java/com/atguigu/TestSSH.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author WEIYUNHUI
+ * @date 2023/12/23 15:45
+ */
+public class TestSSH {
+    public static void main(String[] args) {
+        System.out.println("test ssh");
+    }
+}
